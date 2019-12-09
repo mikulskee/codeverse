7422a692d010efc28d5ac3c1243946f8cc7b11a6
Mateusz Mikulski <mikulskee@gmail.com>
2019-12-09 21:00:31 +0100

    Updates

---
+++ precache-manifest.baaa5c6a5648c1bf15853a689aceffb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "830eb7274669c61cbab573a0aec72dc1",
+    "url": "/codeverse/index.html"
+  },
+  {
+    "revision": "65ff751262533e812d67",
+    "url": "/codeverse/static/css/main.9f2da856.chunk.css"
+  },
+  {
+    "revision": "6c75377954228987c2a3",
+    "url": "/codeverse/static/js/2.64e83c6f.chunk.js"
+  },
+  {
+    "revision": "65ff751262533e812d67",
+    "url": "/codeverse/static/js/main.f42ec67e.chunk.js"
+  },
+  {
+    "revision": "756f4cf751f1532b79c7",
+    "url": "/codeverse/static/js/runtime-main.68d11c0a.js"
+  },
+  {
+    "revision": "a351e03e96a6d02c314c049c2a40a67e",
+    "url": "/codeverse/static/media/main_bg.a351e03e.jpg"
+  }
+]);
