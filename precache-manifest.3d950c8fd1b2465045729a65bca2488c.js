65e059b25a34f3a205b1aa794578bd52a8b842d6
Mateusz Mikulski <mikulskee@gmail.com>
2019-12-12 21:27:35 +0100

    Updates

---
+++ precache-manifest.3d950c8fd1b2465045729a65bca2488c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43f0819294ecffbe8a628d7cc022e5b3",
+    "url": "/codeverse/index.html"
+  },
+  {
+    "revision": "b56eb44e0a50acb0a160",
+    "url": "/codeverse/static/css/main.896f6006.chunk.css"
+  },
+  {
+    "revision": "c8238fc74dce2dff4a11",
+    "url": "/codeverse/static/js/2.773dcddd.chunk.js"
+  },
+  {
+    "revision": "b56eb44e0a50acb0a160",
+    "url": "/codeverse/static/js/main.13d2b9eb.chunk.js"
+  },
+  {
+    "revision": "756f4cf751f1532b79c7",
+    "url": "/codeverse/static/js/runtime-main.68d11c0a.js"
+  },
+  {
+    "revision": "0a2a8ee1df969e26bdc0b162784f4452",
+    "url": "/codeverse/static/media/bg_back.0a2a8ee1.png"
+  },
+  {
+    "revision": "d0b398e52cfb13f3eff1bce2ae89cc3b",
+    "url": "/codeverse/static/media/bg_front.d0b398e5.png"
+  },
+  {
+    "revision": "a1306bd32b2d7109c09ed96558cdabf3",
+    "url": "/codeverse/static/media/project1.a1306bd3.png"
+  },
+  {
+    "revision": "77655b0d672269b25275d122f7b5ea9f",
+    "url": "/codeverse/static/media/project2.77655b0d.png"
+  },
+  {
+    "revision": "25abae1b278bbce854c13bb1f0a86ef0",
+    "url": "/codeverse/static/media/project3.25abae1b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/codeverse/precache-manifest.83dbd79731b3c667bbf15e7c9ac6806b.js"
+  "/codeverse/precache-manifest.3d950c8fd1b2465045729a65bca2488c.js"
 );
 
 self.addEventListener('message', (event) => {
