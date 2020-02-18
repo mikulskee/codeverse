3dfa3e7e4f1032c9d239f4a226ad23968119512e
Mateusz Mikulski <mikulskee@gmail.com>
2020-02-19 00:24:36 +0100

    another heroku setup

--- app.js
+++ app.js
@@ -59,6 +59,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
