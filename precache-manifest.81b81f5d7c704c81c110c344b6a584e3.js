5baa0a22c62fd9021f4f1d4409fef51a6b3eb00e
Mateusz Mikulski <mikulskee@gmail.com>
2020-02-19 20:23:41 +0100

    Updates

---
+++ precache-manifest.81b81f5d7c704c81c110c344b6a584e3.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "847ce1c81e27f2a13b17c3c83635af3c",
+    "url": "/codeverse/index.html"
+  },
+  {
+    "revision": "c2f9ec3a01af596ccf5c",
+    "url": "/codeverse/static/css/main.e3e92313.chunk.css"
+  },
+  {
+    "revision": "369231a3843fe70c33d4",
+    "url": "/codeverse/static/js/2.fb09a1f6.chunk.js"
+  },
+  {
+    "revision": "c2f9ec3a01af596ccf5c",
+    "url": "/codeverse/static/js/main.76080cff.chunk.js"
+  },
+  {
+    "revision": "756f4cf751f1532b79c7",
+    "url": "/codeverse/static/js/runtime-main.68d11c0a.js"
+  },
+  {
+    "revision": "3cb71a1d22bc13509223d6981d6e76bf",
+    "url": "/codeverse/static/media/about-me.3cb71a1d.jpg"
+  },
+  {
+    "revision": "899d21d91a5497ba23a13321da1d87ad",
+    "url": "/codeverse/static/media/about-me2.899d21d9.png"
+  },
+  {
+    "revision": "a3e938177eaba767aabaae06fb459168",
+    "url": "/codeverse/static/media/contact-bg.a3e93817.jpg"
+  },
+  {
+    "revision": "0509ff4da310db9af5ebcdab635872e5",
+    "url": "/codeverse/static/media/grill.0509ff4d.png"
+  },
+  {
+    "revision": "0343e5d9c5af5249c040d19e8848807e",
+    "url": "/codeverse/static/media/header-bg.0343e5d9.jpg"
+  },
+  {
+    "revision": "b7378ea3d0f7aa00eea2cb5f99b8a326",
+    "url": "/codeverse/static/media/logo.b7378ea3.png"
+  },
+  {
+    "revision": "4da45983dd5ee3ae1ea54b5f1813bbaa",
+    "url": "/codeverse/static/media/logo80s.4da45983.png"
+  },
+  {
+    "revision": "30b2fcbaa1ae0a70331915a5193531a9",
+    "url": "/codeverse/static/media/me-and-ben.30b2fcba.jpg"
+  },
+  {
+    "revision": "5382cd3d06f6ca13bb3a2b6f21322f6e",
+    "url": "/codeverse/static/media/project1.5382cd3d.png"
+  },
+  {
+    "revision": "6358bd7ea682b4ba9b85f1df41029f25",
+    "url": "/codeverse/static/media/project2.6358bd7e.png"
+  },
+  {
+    "revision": "542478964708e8e0aadf0ef8e1f430b0",
+    "url": "/codeverse/static/media/project3.54247896.png"
+  },
+  {
+    "revision": "dd5ebf25c48a0ad7c4f0af4576ecf1e6",
+    "url": "/codeverse/static/media/skills-bg.dd5ebf25.jpg"
+  },
+  {
+    "revision": "598673cbe1203bfd7e9cba84e677e89a",
+    "url": "/codeverse/static/media/sticker-6.598673cb.png"
+  },
+  {
+    "revision": "6b8233983712062eecae5a0947f70244",
+    "url": "/codeverse/static/media/triangles.6b823398.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "mikulskee.github.io/codeverse/precache-manifest.0a6b71ae03198386c29daaaec4fd101f.js"
+  "/codeverse/precache-manifest.81b81f5d7c704c81c110c344b6a584e3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("mikulskee.github.io/codeverse/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/codeverse/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
